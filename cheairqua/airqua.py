d34086bdd06882e6553f027f54c5c804d87c9d02
Kahlil Gibran Al Zulmi <76600785+kahlilzulmi@users.noreply.github.com>
2025-06-25 01:20:26 +0700

    Rebuild Django and add index.html

--- cheairqua/airqua.py
+++
@@ -1,9 +0,0 @@
-from openaq import OpenAQ
-
-client = OpenAQ(api_key='2ad5c805534fabe02ef4baa9f0a374d502e9607b02ec080b8323dced2866b25e')
-
-location = client.locations.get(2178)
-
-client.close()
-
-print(location)
