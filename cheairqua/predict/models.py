7a6d56b79ee2b5b6b8162504712d7ba3ddea469c
Kahlil Gibran Al Zulmi <76600785+kahlilzulmi@users.noreply.github.com>
2025-06-25 01:55:54 +0700

    Refactor air quality data handling and update location selection in the frontend

--- cheairqua/predict/models.py
+++ cheairqua/predict/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from openaq import OpenAQ
+import os
+from pandas import json_normalize
+
+API_KEY = os.environ.get('2ad5c805534fabe02ef4baa9f0a374d502e9607b02ec080b8323dced2866b25e')
+
+def air_quality(location_id):
+    client = OpenAQ(api_key=API_KEY)
+    location = client.locations.get(location_id)
+    result = json_normalize(location.dict())
+    client.close()
+    return result
